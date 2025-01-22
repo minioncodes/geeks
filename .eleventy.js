eff0afe42f13d832f75138a4ed7edfd05976899f
Amankhare01 <amankhare.aa@gmail.com>
2025-01-22 15:16:46 +0530

    FAQ

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,4 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/assets/css");
-    eleventyConfig.addPassthroughCopy("./src/assets/js");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/robots.txt");
     eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
